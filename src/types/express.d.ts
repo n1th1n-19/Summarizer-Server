5f749ae240522642c880492506518663be175f9f
n1th1n <r0gc0d3s@gmail.com>
2025-09-01 02:32:15 +0530

    fix3

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,4 +1,5 @@
 import { User } from './user';
+import { Express } from 'express-serve-static-core';
 
 declare global {
   namespace Express {
@@ -7,3 +8,5 @@ declare global {
     }
   }
 }
+
+export {};
